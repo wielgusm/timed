116153a2d57dae9d568f3b118794bd8738a6febc
Maciej Wielgus <mwielgus@MacLap.local>
2017-11-29 11:40:25 -0500

    timed

--- timed/__init__.py
+++ timed/__init__.py
@@ -1,10 +1,6 @@
 """
 .. module:: eat
     :platform: Unix
-    :synopsis: EHT Analysis Toolkit
-
-.. moduleauthor:: Lindy Blackburn (lindylam@gmail.com)
 
 """
-from .factor import *
-from .eht    import *
+
